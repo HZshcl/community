30212d20e28cdccaae0cea711d27850ad4cc6818
huang <1962997472@qq.com>
2021-04-17 16:27:51 +0800

    community

---
+++ src/main/java/communication/cmm/CmmApplication.java
@@ -0,0 +1,13 @@
+package communication.cmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmmApplication.class, args);
+    }
+
+}
